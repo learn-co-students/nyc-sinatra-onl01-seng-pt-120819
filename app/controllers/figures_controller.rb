0b43f6be6fe0719c834b96956e06e1d2a9b9f0b4
RebeccaHA <rebeccahunt034@gmail.com>
2020-03-30 12:38:57 +0100

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,16 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+  @figure = Figure.create(params["figure"])
+
+  redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    erb :'figures/show'
+  end
 end
